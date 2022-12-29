a2e792c539b9f7cdb23613971e0d08f7abcb0271
yaheda <yaheda.a@gmail.com>
2022-12-29 23:56:11 +0200

    connect wallet

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from './modules/axios';
+import { web3Accounts, web3Enable, web3FromSource } from '@polkadot/extension-dapp';
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
+  const [account, setAccount] = useState(undefined);
+  const [secret, setSecret] = useState(undefined);
+  const [error, setError] = useState(undefined);
+
   useEffect(() => {
-    axios.get('api/hello').then(response => {
-      alert(response.data);
-    });
-    
-  });
+    const init = () => {
+      initPolkadot();
+    };
+    init();
+  }, []);
+
+  async function initPolkadot() {
+    setLoading(true);
+
+    try {
+      const extensions = await web3Enable('LitentryTask');
+      const allAccounts = await web3Accounts();
+      if (extensions.length === 0) {
+        setError('no extension installed, or the user did not accept the authorization')
+        return;
+      }
+      debugger;
+      setAccount(allAccounts[0].address);
+    } catch(error) {
+      setError(error.message ?? 'Opps error, please contact your system administrator');
+    }
+
+    setLoading(false);
+  }
 
   async function signin() {
     var data = { ben: 'zona' };
@@ -19,15 +46,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          Litentry Fullstack Task
-        </div>
+        <div>Litentry Fullstack Task</div>
+        {account && <div>Wallet connected with {account}</div>}
+
         <img src={logo} className="App-logo" alt="logo" />
         <button 
           className="btn btn-success"
           onClick={e => signin()}>
             Click here to sign-in with Polkadot and reveal secret
         </button>
+        {error && <p className='text-danger'>{error}</p>}
+        {secret && <p>{secret}</p>}
       </header>
     </div>
   );
