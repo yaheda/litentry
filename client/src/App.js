5164de2bd330e764b9b50c441d91743db33c141f
yaheda <yaheda.a@gmail.com>
2022-12-30 00:23:12 +0200

    sign payload

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, {useEffect, useState} from 'react';
 import axios from './modules/axios';
 import { web3Accounts, web3Enable, web3FromSource } from '@polkadot/extension-dapp';
+import { stringToHex } from "@polkadot/util";
 
 function App() {
 
@@ -30,7 +31,7 @@ function App() {
         return;
       }
       debugger;
-      setAccount(allAccounts[0].address);
+      setAccount(allAccounts[0]);
     } catch(error) {
       setError(error.message ?? 'Opps error, please contact your system administrator');
     }
@@ -38,25 +39,66 @@ function App() {
     setLoading(false);
   }
 
+  async function getSignInPayload() {
+    const injector = await web3FromSource(account.meta.source);
+    const signRaw = injector?.signer?.signRaw;
+
+    if (!!signRaw) {
+        const message = `Sign-in request for address ${account.address}`;
+        const { signature } = await signRaw({
+            address: account.address,
+            data: stringToHex(message),
+            type: 'bytes'
+        });
+
+        return {
+          address: account.address,
+          message: message,
+          signature: signature
+        };
+    }
+
+    return undefined;
+  }
+
   async function signin() {
-    var data = { ben: 'zona' };
-    var response = await axios.post('/api/v1/signin', data, { headers: { Accept: "application/json","Content-Type": "application/json"}});
+    setLoading(true);
+
+    var signInPayload = getSignInPayload();
+    if (!signInPayload) {
+      setError('Error signing message');
+      return;
+    }
+
+    try {
+      var response = await axios.post('/api/v1/signin', signInPayload, { headers: { Accept: "application/json","Content-Type": "application/json"}});
+      setAuthenticated(true);
+    } catch (error) {
+      setError('Error contacting server')
+    }
+
+    setLoading(false);
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <div>Litentry Fullstack Task</div>
-        {account && <div>Wallet connected with {account}</div>}
+        {account && <h6>Wallet connected with {account.address}</h6>}
 
         <img src={logo} className="App-logo" alt="logo" />
-        <button 
-          className="btn btn-success"
-          onClick={e => signin()}>
-            Click here to sign-in with Polkadot and reveal secret
-        </button>
+
+        {account && <>
+          <button 
+            className="btn btn-success"
+            onClick={e => signin()}>
+              Click here to sign-in with Polkadot and reveal secret
+          </button>
+          {secret && <p>{secret}</p>}
+        </>}
+        
         {error && <p className='text-danger'>{error}</p>}
-        {secret && <p>{secret}</p>}
+        
       </header>
     </div>
   );
