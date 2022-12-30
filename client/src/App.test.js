a757203773266c41d9db2d6eb04990643948784d
yaheda <yaheda.a@gmail.com>
2022-12-30 23:22:31 +0200

    testsuite for react

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders <App /> component correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Litentry Fullstack Task/i);
+  expect(element).toBeInTheDocument();
 });
